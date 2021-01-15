e4abee49ebbb846cffa5f95dcca875538fbe1ab8
Carlos Henrique <carlos.silva@novaandradina.org>
2021-01-14 20:01:43 -0400

    add vue structs

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <Landing />
+  </v-app>
+</template>
+
+<script>
+import Landing from  './views/Landing';
+
+export default {
+  name: 'App',
+  components: {
+    Landing,
+  }
+};
+</script>
